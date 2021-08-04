41bdfca825249de7d55586e5c795319b911a786d
Ivaha-ha <rentrooms.lviv.ua@gmail.com>
2021-08-04 18:02:38 +0300

    temperature from accuweather

--- weatherapp2.py
+++ weatherapp2.py
@@ -1,7 +1,41 @@
 """
-weather app progect.
-This is my first project.
-I hope it will work!!!
+weather app progect!!!
 """
+ACCU_URL = 'https://www.accuweather.com/uk/ua/lviv/324561/weather-forecast/324561'
+import html
+from urllib.request import urlopen, Request
 
-print("Hello, i'm alive!")
+#getting page from server
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
+accu_request = Request(ACCU_URL, headers=headers)
+accu_page = urlopen(accu_request).read()
+accu_page = str(accu_page)
+
+ACCU_TEMP_TAG = '<div class="temp">'
+print(accu_page.find(ACCU_TEMP_TAG))
+"""
+accu_temp_start = accu_page.find(ACCU_TEMP_TAG)+len(ACCU_TEMP_TAG)
+accu_temp_finish = accu_temp_start + 4
+print(accu_page[accu_temp_start : accu_temp_finish])
+"""
+accu_temp_tag_size = len(ACCU_TEMP_TAG)
+accu_temp_tad_index = accu_page.find(ACCU_TEMP_TAG)
+accu_temp_value_start = accu_temp_tad_index + accu_temp_tag_size
+accu_temp = ''
+for char in accu_page[accu_temp_value_start:]:
+    if char != '<':
+        accu_temp += char
+    else:
+        break
+print('ACCUWEATHER:\n')
+print(f'Temperature: {html.unescape(accu_temp)}')
+
+#print(accu_page)
+
+
+
+
+
+
+
+
